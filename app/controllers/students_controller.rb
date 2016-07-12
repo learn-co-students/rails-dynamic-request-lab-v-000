d7dcdc2dff9738f412624756da0e46988a9882fb
Christian Whitesides <whitesidescr@gmail.com>
2016-07-12 00:24:43 -0600

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,9 +1,18 @@
 class StudentsController < ApplicationController
+  # before_action :set_student, only: :show
+
   def index
     @students = Student.all
   end
 
   def show
     @student = Student.find(params[:id])
+    # The same thing can be achieved by doing what is commented out (above + below).
   end
+
+  # private
+
+  # def set_student
+  #   @student = Student.find(params[:id])
+  # end
 end
