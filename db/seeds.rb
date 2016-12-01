c7e43e3994f53c3310f3feb243f96225045bba52
Aya Fushimi <aya.fushimi@gmail.com>
2016-12-01 20:46:32 +0000

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+Student.create!(first_name: "Daenerys", last_name: "Targaryen")
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
