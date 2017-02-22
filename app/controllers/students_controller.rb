4984b6a69e56cf798de713811971ce4f2b251d66
nicolefederici <nicolefederici@yahoo.com>
2017-02-21 22:49:04 -0500

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -2,4 +2,41 @@ class StudentsController < ApplicationController
   def index
     @students = Student.all
   end
+
+  def new
+    @student = Student.new
+  end
+
+  def create
+    @student = Student.new params[:student]
+    if @student.save
+      redirect_to posts_path
+    else
+      render action: 'new'
+    end
+  end
+
+  def edit
+    @student = Student.find params[:id]
+  end
+
+  def update
+    @student = Student.find params[:id]
+    if @student.update_attributes params[:student]
+    redirect_to students_path
+  else
+    render action: :edit 
+  end
+end
+
+def destroy
+   @student = Student.find params[:id]
+   @student.destroy
+   redirect_to students_path
+ end
+
+ def show
+  @student = Student.find params[:id]
+end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
-  resources :students, only: :index
+  resources :students
+  get '/all', to: 'students#index', as: 'all'
 end
