1b22bfa50066a77d6f108fb0d9676465035d0954
WilliamBarela <william.barela.dev@gmail.com>
2017-06-26 11:39:29 +0900

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
-  resources :students, only: :index
+  # This is Rails way of doing things RESTfully
+  resources :students, only: [:index, :show]
+
+  # equivalent to:
+  # get 'students' to: 'students/index'
+  # get 'students/:id' to: 'students/show'
+
+  # also, equivalent to:
+  # get '/students' => 'students/index'
+  # get '/students/:id' => 'students/show'
+  
+  # So, basically, the routes are like this:
+  # HTML_VERB "ROUTE(static or dynamic)" (to: / =>) "TEMPLATE_LOCATION"
 end
