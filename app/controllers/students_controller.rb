237aece557794be9d2917b801d95ee503e0b9e0e
Kevin Yongchul Kim <kevikim33@gmail.com>
2017-10-18 20:55:20 -0700

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -8,3 +8,21 @@ class StudentsController < ApplicationController
   end
 
 end
+
+# ALTERNATE SOLUTION VERSION
+# class StudentsController < ApplicationController
+#   before_action :set_student, only: :show
+#
+#   def index
+#     @students = Student.all
+#   end
+#
+#   def show
+#   end
+#
+#   private
+#
+#     def set_student
+#       @student = Student.find(params[:id])
+#     end
+# end
