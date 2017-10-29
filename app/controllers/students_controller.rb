05996cb8dded6e4b02b328866e490c01a32e7d10
github username <github email address>
2017-10-29 13:36:12 -0400

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -2,4 +2,17 @@ class StudentsController < ApplicationController
   def index
     @students = Student.all
   end
+
+  def show
+    @student = Student.find(params[:id])
+  end
 end
+
+# @students stores array of all student instances to pass to view file index.html.erb
+# Implicitly render views/students/index.html.erb view file
+# b/c it has the same name as the #index action(method) that's being called, defined in StudentsController
+
+# @student stores student instance found by its @id attribute value, which = params[:id],
+# i.e., whatever the user passed into URL to replace :id route variable
+# implicitly render the views/students/show.html.erb view file
+# b/c it has the same name as the #show action(method) being called, defined in StudentsController
