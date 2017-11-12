24e2b99fe31a02da69818630c97117e151f065af
Deborah Seow <deborahseow9@gmail.com>
2017-11-12 00:28:33 +0000

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -2,4 +2,8 @@ class StudentsController < ApplicationController
   def index
     @students = Student.all
   end
+
+  def show
+    @student = Student.find_by(first_name: "Daenerys", last_name: "Targaryen")
+  end
 end
