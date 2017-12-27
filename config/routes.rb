1b86327a36ca5aa3554010838aa249cc7512551c
miriam <miriam5000@gmail.com>
2017-12-27 11:40:03 -0500

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
+# Rails.application.routes.draw do
+#   # resources :students, only: :index
+#   resources :students, only: [index, show]
+#   # get '/students/:id' => 'students#show'
+#   # resources :students, only: :show 
+# end
+
+
 Rails.application.routes.draw do
-  resources :students, only: :index
+  resources :students, only: [:index, :show]
 end
+
