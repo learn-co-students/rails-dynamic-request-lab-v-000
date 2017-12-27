c12d40c19b9e33f6b75ab60399fc5ea4cd4b8646
miriam <miriam5000@gmail.com>
2017-12-27 12:30:26 -0500

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,14 +1,24 @@
 class StudentsController < ApplicationController
+	before_action :set_student, only: :show
+
   def index
     @students = Student.all
   end
 
   def show 
-  	# @student = Student.create!(:first_name => params[:first_name], :last_name => params[:last_name] )
-  	@student = Student.find_by(params[:id])
+ 
   end 
+
+  private
+
+  	def set_student
+  		@student = Student.find(params[:id])
+  	end 
 
 end
 
 #traffic cop == controller
 #model creates the instance 
+
+ 	# @student = Student.create!(:first_name => params[:first_name], :last_name => params[:last_name] )
+  # 	@student = Student.find_by(params[:id])
--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@
 
 
 Rails.application.routes.draw do
+	# get "students/:id", to: "students#show"
   resources :students, only: [:index, :show]
 end
 
