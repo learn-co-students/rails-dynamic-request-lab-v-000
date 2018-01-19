7fecb5ccca705f000163b0411bc2b9e10337ecca
Anastasia  Barkett <anastasia.barkett@gmail.com>
2018-01-19 22:32:11 +0000

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+students = Student.create([{ first_name: 'Charlie', last_name: 'Brown'}, {first_name: 'Linus', last_name: 'Van Pelt'}])
