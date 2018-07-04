73c6533f93df92e66c14976385a6a8e081cf63da
Jayon Kim <jpkim921@gmail.com>
2018-07-04 14:09:06 +0000

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
-  resources :students, only: :index
-  get 'students/:id', to: 'students#show'
+  # resources :students, only: :index
+  # get 'students/:id', to: 'students#show'
+
+  # example of Resource routing
+  resources :students
 end
