ccac81b557e9105012b116849e628834d4935465
Dillorom Abdulloeva <dabdulloeva@gmail.com>
2018-07-26 21:48:07 +0000

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -2,4 +2,9 @@ class StudentsController < ApplicationController
   def index
     @students = Student.all
   end
+
+  def show
+    @student = Student.create!(first_name: params[:first_name], last_name: params[:last_name])
+    @student = Student.find_by_id(params[:id])
+  end
 end
